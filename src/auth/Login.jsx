1570719fa98c080fb3b66256f9e7a0fdfc8ca6f6
Lourdhu02 <b.lourdhuraju1234@gmail.com>
2024-02-23 11:14:39 +0530

    first commit

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import "./styles.css";
+import loginImg from "../assets/reshot-illustration-social-media-manager-R48ZCSE7KP.png"
+import { useNavigate } from 'react-router-dom';
 const Login = () => {
-  
+  const navigate = useNavigate();
+  const handleLoginClick = () => {
+    navigate('/home');
+  }
   
 
   return (
     <div className='signup-component'>
-      <div className='left-content'></div>
+      <div className='left-content'>
+        <img src={loginImg} className="loginImg" alt="loginImg" />
+      </div>
       <div className='right-content'>
       <form>
         <label>
@@ -20,11 +27,12 @@ const Login = () => {
          
         </label>
          <input type="password" name="password"  />
-        <button type="submit">Login</button>
+         <div className="bot_sign"><div>Don`t have an account?</div><Link to="/signup" className="botlink">SignUp</Link>  </div>
+        <button type="submit" onClick={handleLoginClick}>Login</button>
       </form>
       <div>
       <span className="loginForgot">Forgot Password?</span>
-      <div className="bot_sign"><div>Don`t have an account?</div><Link to="/signup" className="botlink">SignUp</Link>  </div>
+      
       </div>
       </div>
     </div>
